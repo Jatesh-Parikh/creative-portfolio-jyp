29eed6735ad72fa9796583592cb1f3d485079859
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-10 20:19:46 +0530

    add biography slice

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,6 +1,10 @@
-import { FC } from "react";
+import Avatar from "@/components/Avatar";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Biography`.
@@ -10,20 +14,31 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
 /**
  * Component for "Biography" Slices.
  */
-const Biography: FC<BiographyProps> = ({ slice }) => {
+const Biography = ({ slice }: BiographyProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for biography (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+        <Heading size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+        <Button 
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
+        />
+
+        <Avatar 
+          image={slice.primary.avatar}
+          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
+        />
+      </div>
+    </Bounded>
   );
 };
 
