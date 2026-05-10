f2ebff49803c01b294b7aaeb0921811dd7659787
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-10 18:47:32 +0530

    add root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,24 @@
+import "./globals.css"
+import clsx from "clsx";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: settings.data.meta_title,
+    description: settings.data.meta_description,
+  }
+}
 
 export default function RootLayout({
   children,
@@ -7,9 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+    <html lang="en" className="bg-slate-900">
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
+        <Header />
+        {children}
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light" />
+        <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }
