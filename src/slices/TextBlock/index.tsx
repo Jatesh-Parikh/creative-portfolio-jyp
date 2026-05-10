268e678ba5197fc2bb6d5ee64a7b9f4eb298daff
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-10 20:12:15 +0530

    add TextBlock slice

--- src/slices/TextBlock/index.tsx
+++ src/slices/TextBlock/index.tsx
@@ -1,6 +1,5 @@
-import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `TextBlock`.
@@ -10,21 +9,11 @@ export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
 /**
  * Component for "TextBlock" Slices.
  */
-const TextBlock: FC<TextBlockProps> = ({ slice }) => {
+const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for text_block (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+    <div className="max-w-prose">
+      <PrismicRichText field={slice.primary.text} />
+    </div>
   );
 };
 
